d5ad423c9387e455f3d631f518634dd0aaa48cf8
Ariel <borochov@american.edu>
2014-12-30 23:30:03 -0500

    some changes

--- app/src/main/java/com/software/arielb/blogreader/MainListActivity.java
+++ app/src/main/java/com/software/arielb/blogreader/MainListActivity.java
@@ -1,26 +1,49 @@
 package com.software.arielb.blogreader;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.res.Resources;
-import android.support.v7.app.ActionBarActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainListActivity extends ListActivity {
-    protected String [] mAndroidNames;
+    protected String [] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS=20;
+    public static final String TAG=MainListActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
-        Resources resources=getResources();
-        mAndroidNames=resources.getStringArray(R.array.android_names);
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mAndroidNames);
-        setListAdapter(adapter);
+        if (isNetworkAvailable()) {
+            GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+            getBlogPostsTask.execute();
+        }else {
+            Toast.makeText(this,"Network not available", Toast.LENGTH_LONG).show();
+        }
+    }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo=manager.getActiveNetworkInfo();
+
+        boolean isAvailable =false;
+        if (networkInfo!=null && networkInfo.isConnected()){
+            isAvailable=true;
+        }
+        return isAvailable;
     }
 
 
@@ -44,5 +67,33 @@ public class MainListActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetBlogPostsTask extends AsyncTask<Object,Void,String>{
+
+        @Override
+        protected String doInBackground(Object[] params) {
+            int httpResponse=-1;
+            try {
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POSTS);
+                HttpURLConnection connection= (HttpURLConnection)blogFeedUrl.openConnection();
+                connection.connect();
+                httpResponse=connection.getResponseCode();
+                if (httpResponse==HttpURLConnection.HTTP_OK){
+
+                }else{
+                    Log.i(TAG,"Unsuccessful Code : "+httpResponse);
+                }
+            }
+            catch (MalformedURLException e){
+                Log.e(TAG,"Malfomed Exception Caught: ",e);
+            }catch (IOException e){
+                Log.e(TAG, "IO Excpetion cahgt: ",e);
+
+            }catch (Exception e){
+
+            }
+            return "code"+httpResponse;
+        }
     }
 }
